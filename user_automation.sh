758c2c5257f727ebbd65facc2e81282bf6281084
Raju_Hirehal <myemailaddressisraju@gmail.com>
2022-09-21 02:59:26 +0530

    added userautomation script

---
+++ user_automation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ $# -gt 0 ]
+then
+    USERS=$@
+    for USER in ${USERS[@]}
+    do
+        echo $USERS
+    done
+else
+    echo "Please enter valid input"
+fi
+
+
+#user_automation.sh testuser1 testuser2 testuser3 testuser4 testuser5
