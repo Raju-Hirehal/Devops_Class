f04190107d8c49055610cea1c6d08c5996c8ff3c
Raju_Hirehal <myemailaddressisraju@gmail.com>
2022-09-21 04:12:45 +0530

    adding existing user

--- user_automation.sh
+++ user_automation.sh
@@ -4,11 +4,14 @@ then
     USERS=$@
     for USER in ${USERS[@]}
     do
-        echo $USERS
+    EXISTING_USER=$(cat /etc/passwd | grep -i $USER | cut -d ":" -f1)
+    if [ "${EXISTING_USER}" = "${USER} ]
+    then
+    echo "User name $USER already exists tra a different name"
+    else
+    echo "lets create $USER"
+    fi
     done
 else
     echo "Please enter valid input"
 fi
-
-
-#user_automation.sh testuser1 testuser2 testuser3 testuser4 testuser5
