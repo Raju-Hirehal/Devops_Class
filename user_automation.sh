60b6649b93a7ebefb9451b4bc3bd083c9d95fb44
Raju_Hirehal <myemailaddressisraju@gmail.com>
2022-09-22 00:22:30 +0530

    added user

--- user_automation.sh
+++ user_automation.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-if [ $# -gt 0 ]
-then
+if [ $# -gt 0 ]; then
     USERS=$@
-    for USER in ${USERS[@]}
-    do
-        echo $USER
+    for USER in ${USERS[@]}; do
+        EXISTING_USER=$(cat /etc/passwd | grep -i ubuntu | cut -d ":" -f1)
+        if [ "${EXISTING_USER}" = "${USER}" ]; then
+            echo "USERNAME $USER ALREADY EXISTS TRY A DIFFERENT NAME"
+        else
+            echo "Lets crate user $USER"
+        fi
     done
 else
     echo "Please enter valid input"
 fi
-
 
 #user_automation.sh testuser1 testuser2 testuser3 testuser4 testuser5
